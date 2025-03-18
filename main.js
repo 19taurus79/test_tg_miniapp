5e4b6936b8eabadc45307e849094ff1221f71e3a
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2025-03-18 17:49:02 +0200

    add css

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const tg = window.Telegram.Webapp;
 console.log(tg);
-const btn = document.querySelector("#button");
-btn.addEventListener("click", () => {
-  tg.openChat("@EridonKharkiv_bot");
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", (event) => {
+  console.log(tg.initData);
 });
